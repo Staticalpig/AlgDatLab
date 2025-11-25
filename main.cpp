4c14b7920a78d7a329106869ba1e959ae9f61d72
Tim G. <knarklunga@live.se>
2025-11-25 22:13:24 +0100

    Added insertionSort and some test printing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <vector>
 
+void insertionSort(std::vector<int> &v);
+void printVector(const std::vector<int> &v);
+
 
 std::vector<int> BucketSort(std::vector<int> &v) {
     if (v.empty()) {
@@ -34,5 +37,32 @@ int main() {
     }
     std::cout << std::endl;
 
+    //testing
+    //printVector(sorted);
+    insertionSort(sorted);
+    printVector(sorted);
+
     return 0;
+}
+
+void insertionSort(std::vector<int> &v) {
+    for (int i = 1; i < v.size(); i++) {
+        int key = v[i];
+        int j = i - 1;
+
+        while (j >= 0 && v[j] > key) {
+            //std::cout << std::format("Swapping {} and {} at v[{}]\n", v[j], v[j + 1], i);
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = key;
+        printVector(v);
+    }
+}
+
+void printVector(const std::vector<int> &v) {
+    for (const int i : v) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
 }
