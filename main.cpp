74bcfb488954ef63498585dcd0bda3b2bfd592cc
Tim G. <knarklunga@live.se>
2025-11-29 17:06:49 +0100

    algo complexity calculated?

--- main.cpp
+++ main.cpp
@@ -5,29 +5,12 @@
 
 void insertionSort(std::vector<int> &v);
 void printVector(const std::vector<int> &v);
-
-
-std::vector<int> BucketSort(std::vector<int> &v) {
-    if (v.empty()) {
-        return {};
-    }
-
-    const int k = *std::ranges::max_element(v);
-
-    std::vector<int> w = std::vector<int>(k + 1, 0);
-
-    for (const int i : v) {
-        w[i] +=1;
-    }
-
-    return w;
-}
+std::vector<int> BucketSort(std::vector<int> &v);
 
 
 int main() {
-
     std::vector<int> data = {20,20,1,25, 0, 0, 0, 0};
-    std::vector<int> sorted = BucketSort(data);
+    std::vector<int> sorted = BucketSort(data); //0(n + k) from BucketSort
 
     std::cout << sorted.size() << std::endl;
     std::cout << "[POS]:[AMOUNT]" << std::endl;
@@ -39,18 +22,34 @@ int main() {
 
     //testing
     //printVector(sorted);
-    insertionSort(sorted);
+    insertionSort(sorted);      //gets 'sorted' which is size of k, insertion sort is O(k^2) in worst case. O(k^2) > O(n + k)
     printVector(sorted);
 
     return 0;
 }
 
+std::vector<int> BucketSort(std::vector<int> &v) {
+    if (v.empty()) {
+        return {};
+    }
+
+    const int k = *std::ranges::max_element(v);         //O(n)
+
+    std::vector<int> w = std::vector<int>(k + 1, 0);    //O(k)
+
+    for (const int i : v) {                             //O(n)
+        w[i] +=1;
+    }
+
+    return w;                                           //Overall complexity: O(n + k)
+}
+
 void insertionSort(std::vector<int> &v) {
-    for (int i = 1; i < v.size(); i++) {
+    for (int i = 1; i < v.size(); i++) {    //v size is k+1
         int key = v[i];
         int j = i - 1;
 
-        while (j >= 0 && v[j] > key) {
+        while (j >= 0 && v[j] > key) {      //k again, so O(k^2) overall?
             //std::cout << std::format("Swapping {} and {} at v[{}]\n", v[j], v[j + 1], i);
             v[j + 1] = v[j];
             j--;
