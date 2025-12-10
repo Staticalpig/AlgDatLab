d12f7ff29db185c235bc2ee5361760a55280b186
Tim G. <knarklunga@live.se>
2025-12-10 20:17:56 +0100

    Updated BucketSort. Should be finished now.

--- part_1/main.cpp
+++ part_1/main.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <format>
 #include <iostream>
 #include <vector>
 
@@ -7,11 +6,32 @@ void insertionSort(std::vector<int> &v);
 void printVector(const std::vector<int> &v);
 std::vector<int> BucketSort(std::vector<int> &v);
 
-
 int main() {
-    std::vector v = {10, 1, 12, 1, 10, 1, 5};    //previously called "data"
-    std::vector<int> w = BucketSort(v); //0(n + k) from BucketSort. previously called "sorted"
+    //the original vector used as input
+    std::vector v = {10, 1, 12, 1, 10, 1, 5};
+    std::cout << "Original vector 'v': \n";
+    printVector(v);
+    std::vector<int> w = BucketSort(v);
 
+    insertionSort(w);
+    //printVector(w);
+    return 0;
+}
+
+std::vector<int> BucketSort(std::vector<int> &v) {
+    if (v.empty()) {
+        return {};
+    }
+    //find the biggest element in v, and create a vector w of size (k+1) initialized to 0
+    const int k = *std::ranges::max_element(v);
+    std::vector<int> w = std::vector(k + 1, 0);
+
+    //count frequency of each element in v by increasing the corresponding index in w
+    for (const int i: v) {
+        w[i] += 1;
+    }
+
+    //print out v in a sorted order using w
     std::cout << "    Use 'w' to write out the vector 'v' in a sorted order:" << std::endl;
     for (int i = 0; i < w.size(); i++) {
         for(int j = w[i]; j > 0; j--){
@@ -19,42 +39,19 @@ int main() {
         }
     }
     std::cout << std::endl;
-    //testing
-    printVector(w);
-    insertionSort(w); //gets 'sorted' which is size of k, insertion sort is O(k^2) in worst case. O(k^2) > O(n + k)
-    printVector(w);
-
-    return 0;
-}
-
-std::vector<int> BucketSort(std::vector<int> &v) {
-    if (v.empty()) {
-        // O(1)
-        return {};
-    }
-    const int k = *std::ranges::max_element(v); //O(n)
-    std::vector<int> w = std::vector(k + 1, 0); //O(k)
-
-    for (const int i: v) {
-        //O(n)
-        w[i] += 1; //O(1) ish O(1N)
-    }
-    return w; //Overall complexity: O(n + k)
+    return w;
 }
 
 void insertionSort(std::vector<int> &v) {
     for (int i = 1; i < v.size(); i++) {
-        //v size is k+1
         const int key = v[i];
         int j = i - 1;
 
         while (j >= 0 && v[j] > key) {
-            //k again, so O(k^2) overall?
             v[j + 1] = v[j];
             j--;
         }
-        v[j + 1] = key; // O(1) ?
-        //printVector(v);
+        v[j + 1] = key;
     }
 }
 
